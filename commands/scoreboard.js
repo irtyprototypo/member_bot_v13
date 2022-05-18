5c90b104e277a20c7b7406119ab2a2148160fc10
irtypo <irtypo@gmail.com>
2022-05-17 21:26:15 -0500

    scoreboard rename fix

--- commands/scoreboard.js
+++ commands/scoreboard.js
@@ -4,7 +4,6 @@ const { checkPoints } = require('../util.js');
 const { MODE } = require('../config/bot.json');
 // const CHANNEL_TEST = (MODE == 'DEV') ? channel_bot_testing_alt : channel_bot_testing;
 // const CHANNEL_YELL = (MODE == 'DEV') ? channel_bot_spam : channel_bot_spam_alt;
-
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('scoreboard')
@@ -15,15 +14,15 @@ module.exports = {
 
 		csv.forEach((g, i) => {
 			interaction.guild.members.cache.forEach( m =>{
-				if(m.user.username.endsWith('🏆'))
-					m.user.username.slice(0, -1);
+				if(m.displayName.endsWith('🏆'))
+					m.displayName.substr(0, m.displayName.length-2);
 			});
 
 			switch(parseInt(g.place)){
 				case 1:
 					str = `🥇 ${g.place}st: ${g.username} 🏆`;
 					interaction.guild.members.fetch(g.id).then( e =>{
-							if(!e.nickname.endsWith('🏆'))
+							if(e.nickname && !e.nickname.endsWith('🏆'))
 								if(MODE != 'DEV')
 									e.setNickname(`${e.nickname} 🏆`);
 					});
